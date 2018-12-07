9aa21cd88f7ade52aaefa12567833ff5daf8e755
Kuei Peng <kuei_peng@mayohr.com>
2018-12-07 11:13:50 +0800

    add test cases

---
+++ TestDemo.Test/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestDemo.Test
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void 驗證Method回傳結果()
+        {
+            var target = new Calculator();
+            var actual = target.Sum(1 , 2);
+
+            var expected = 3;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void 驗證Exception()
+        {
+            var target = new Calculator();
+
+            var ex = Assert.Throws<DivideByZeroException>(() => target.Divided(1 , 0));
+        }
+    }
+}
---
+++ TestDemo/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDemo
+{
+    public class Calculator
+    {
+        public int Sum(int i1 , int i2) { return i1 + i2; }
+
+        public double Divided(int i1 , int i2) { return i1 / i2; }
+    }
+}
