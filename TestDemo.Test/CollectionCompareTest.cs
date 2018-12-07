ea505aa7eeed2d8a54508704fa86a1e0d93fdeeb
Kuei Peng <kuei_peng@mayohr.com>
2018-12-07 13:34:38 +0800

    add test case ListOfClass_Partial

--- TestDemo.Test/CollectionCompareTest.cs
+++ TestDemo.Test/CollectionCompareTest.cs
@@ -17,7 +17,7 @@ namespace TestDemo.Test
             CollectionAssert.AreEqual(actual , expected);
         }
 
-        [Test]
+        [Test]  // ExpectedObjects reference: https://github.com/derekgreer/expectedObjects/wiki
         public void ListOfInt_UnOrder()
         {
             var actual = new List<int> { 2 , 1 , 3 , 4 , 5 };
@@ -68,6 +68,42 @@ namespace TestDemo.Test
 
             //CollectionAssert.AreEqual(actual , expected);
             expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+        [Test]
+        public void ListOfClass_Partial()
+        {
+            var actual = new List<CustomClass>
+                         {
+                             new CustomClass { Id = 1 , Name = "A" }
+                           , new CustomClass { Id = 2 , Name = "B" }
+                           , new CustomClass { Id = 3 , Name = "C" }
+                         };
+
+            //var expected = new List<CustomClass>
+            //               {
+            //                   new CustomClass { Id = 2 }
+            //                 , new CustomClass { Id = 1 }
+            //                 , new CustomClass { Id = 3 }
+            //               };
+
+
+
+            //var expected = new []
+            //               {
+            //                   new CustomClass { Id = 2 }
+            //                 , new CustomClass { Id = 1 }
+            //                 , new CustomClass { Id = 3 }
+            //               };
+
+            var expected = new []
+                           {
+                               new { Id = 2 }
+                             , new { Id = 1 }
+                             , new { Id = 3 }
+                           };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
         }
     }
 }
