b1bac44c4354df4ac2982a339fd50421c7adb4ab
Kuei Peng <kuei_peng@mayohr.com>
2018-12-07 11:23:36 +0800

    add test case : TestCase使用方式1

--- TestDemo.Test/NUnitDemoTest.cs
+++ TestDemo.Test/NUnitDemoTest.cs
@@ -32,5 +32,22 @@ namespace TestDemo.Test
             var ex = Assert.Throws<CustomException>(() => target.ThrowException("test"));
             Assert.AreEqual("Detail:test", ex.Detail);
         }
+
+        [Test]
+        [TestCase(1)]
+        public void TestCase使用方式1_true(int i)
+        {
+            Assert.AreEqual(1, i);
+        }
+
+        [Test]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void TestCase使用方式1_false(int i)
+        {
+            Assert.AreEqual(false, i == 1);
+        }
     }
 }
